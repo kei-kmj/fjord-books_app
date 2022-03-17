1670ecd30720ac6dc32d5aed5255046fb353af69
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-03-17 17:22:32 +0900

    allow parameters added to the user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     books_path(resource)
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update,keys: [:name, :post_code, :address, :about_me])
+  end
 end
