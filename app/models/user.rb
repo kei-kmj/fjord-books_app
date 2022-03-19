b83b09b178a6f586dd183c7e3c1b3cf8b7069410
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-03-19 15:45:26 +0900

    display image on edit view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :image
+  validate :image_content_type, if: :was_attached?
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def was_attached?
+    self.image.attached?
+  end
+
+  def image_content_type
+    extension = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif']
+    errors.add(:image, "の拡張子が有効ではありません。.png/.jpg/.gif") unless image.content_type.in?(extension)
+  end
+
 end
