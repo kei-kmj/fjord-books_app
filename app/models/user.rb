272054255d987bc8dfba038abaf1ae1f96ad117c
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-03-21 11:07:07 +0900

    Implemented follow button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,28 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :follower, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, class_name: "Follow", foreign_key: "following_id", dependent: :destroy
+  has_many :follower_user, through: :follower, source: :following # I'm following
+  has_many :following_user, through: :following, source: :follower # My follower
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(following_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(following_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 end
