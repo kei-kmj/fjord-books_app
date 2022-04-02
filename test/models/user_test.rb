1041e8786d49c16d70daa3259cce950129f5993c
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-04-02 21:13:58 +0900

    implemented user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#name_or_email" do
+    user = User.new(email: 'foo@sample.com', name: '')
+    assert_equal 'foo@sample.com', user.name_or_email
+
+    user.name = 'piyo'
+    assert_equal 'piyo', user.name_or_email
+  end
+
+  test '#follow_and_unfollow' do
+    ben = User.create!(email: 'ben@example.com', password: 'benben')
+    tom = User.create!(email: 'tom@example.com', password: 'tomtom')
+
+    tom.follow(ben)
+    assert tom.following?(ben)
+    assert ben.followed_by?(tom)
+    tom.unfollow(ben)
+    assert_not tom.following?(ben)
+    assert_not ben.followed_by?(tom)
+  end
 end
