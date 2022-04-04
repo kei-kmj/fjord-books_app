99f46c8761fc36e4d900f1da382f160eb61603e2
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-04-04 18:02:59 +0900

    use partial for view of comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :reports, dependent: :nullify
+  has_many :reports, dependent: :destroy
 
   has_many :comments, dependent: :nullify
 
